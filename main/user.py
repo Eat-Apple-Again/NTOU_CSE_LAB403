25af53e780648677d203ad81adf473854148f9bc
Eat-Apple-Again <00757044@email.ntou.edu.tw>
2023-10-24 20:57:36 +0800

    Add button for resetting the feeding command.

--- main/user.py
+++ main/user.py
@@ -9,19 +9,23 @@ def run_inone():
     subprocess.Popen(["python", "/home/pi/Desktop/NTOU_CSE_LAB403/main/upload.py"])
 def run_toESP32():
     subprocess.Popen(["python", "/home/pi/Desktop/NTOU_CSE_LAB403/RPi2Arduino/fetch_to_arduino.py"])
+def run_reset_command():
+    subprocess.Popen(["python", "/home/pi/Desktop/NTOU_CSE_LAB403/main/modify_decision.py"])
 def stop_program():
     root.destroy()
 
 
 button1 = tk.Button(root, text="查看本地監視器", command=run_monitor)
 button2 = tk.Button(root, text="上傳監視器影像", command=run_inone)
-button3 = tk.bitton(root, text="傳遞指令給ESP32", command=run_toESP32)
-button4 = tk.Button(root, text="關閉", command=stop_program)
+button3 = tk.button(root, text="傳遞指令給ESP32", command=run_toESP32)
+button4 = tk.button(root, text="重設GCP VM DB的投餌指令", command=run_reset_command)
+button5 = tk.Button(root, text="關閉", command=stop_program)
 
 
 button1.pack()
 button2.pack()
 button3.pack()
 button4.pack()
+button5.pack()
 
 root.mainloop()
