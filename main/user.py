a324da00fb052a21957f7e4c305b5fed37c487cc
Eat-Apple-Again <00757044@email.ntou.edu.tw>
2023-10-30 20:30:17 +0800

    Add button for recording video v2.

--- main/user.py
+++ main/user.py
@@ -13,6 +13,8 @@ def run_reset_command():
     subprocess.Popen(["python", "/home/pi/Desktop/NTOU_CSE_LAB403/main/modify_decision.py"])
 def run_recording():
     subprocess.Popen(["python", "/home/pi/Desktop/NTOU_CSE_LAB403/main/video/recording.py"])
+def run_recording_perSecond():
+    subprocess.Popen(["python", "/home/pi/Desktop/NTOU_CSE_LAB403/main/video/recording_perSecond.py"])  
 def stop_program():
     root.destroy()
 
@@ -22,7 +24,8 @@ button2 = tk.Button(root, text="上傳監視器影像", command=run_inone)
 button3 = tk.Button(root, text="傳遞指令給ESP32", command=run_toESP32)
 button4 = tk.Button(root, text="重設GCP VM DB的投餌指令", command=run_reset_command)
 button5 = tk.Button(root, text="開始錄製v1", command=run_recording)
-button6 = tk.Button(root, text="關閉", command=stop_program)
+button6 = tk.Button(root, text="開始錄製v2", command=run_recording_perSecond)
+button7 = tk.Button(root, text="關閉", command=stop_program)
 
 
 button1.pack()
@@ -31,5 +34,6 @@ button3.pack()
 button4.pack()
 button5.pack()
 button6.pack()
+button7.pack()
 
 root.mainloop()
