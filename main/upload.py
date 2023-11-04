63984bc9043a1a7f05d2b0746d99bb286ac02ad4
Eat-Apple-Again <00757044@email.ntou.edu.tw>
2023-11-05 04:40:58 +0800

    Adjusting indentation.

--- main/upload.py
+++ main/upload.py
@@ -47,52 +47,52 @@ def select_data(id):
     return result
 
 if __name__ == '__main__':
-    #選擇攝影機
-    #參考 https://www.ispyconnect.com/camera/d-link
-    cap1 = cv2.VideoCapture('rtsp://Admin:1234@192.168.7.21/cam0/h264')
-    cap1.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
+  #選擇攝影機
+  #參考 https://www.ispyconnect.com/camera/d-link
+  cap1 = cv2.VideoCapture('rtsp://Admin:1234@192.168.7.21/cam0/h264')
+  cap1.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
 
-    i = 0
-    count = 0
-    while(True):
-      # 從攝影機擷取一張影像
-      ret1, frame1 = cap1.read()
-      #resize to 720*480
-      if ret1:
-        frame1 = cv2.resize(frame1,(720,480))
-      else:
-        break
-      if i == 0:
-        print(frame1.shape)
-        i = 1
-      
-      # 顯示圖片(720, 480, 3)
-      cv2.imshow('monitor', frame1)
+  i = 0
+  count = 0
+  while(True):
+    # 從攝影機擷取一張影像
+    ret1, frame1 = cap1.read()
+    #resize to 720*480
+    if ret1:
+      frame1 = cv2.resize(frame1,(720,480))
+    else:
+      break
+    if i == 0:
+      print(frame1.shape)
+      i = 1
+    
+    # 顯示圖片(720, 480, 3)
+    cv2.imshow('monitor', frame1)
 
-      #cv2.imwrite("/home/pi/Desktop/newGCP/kuaikuai01.png", frame1)
+    #cv2.imwrite("/home/pi/Desktop/newGCP/kuaikuai01.png", frame1)
 
-      # 若按下 q 鍵則離開迴圈
-      if cv2.waitKey(1000) & 0xFF == ord('q'):
-        break
-      else:
-        cv2.imwrite("/home/pi/Desktop/NTOU_CSE_LAB403/main/upload_frames/upload.png", frame1)
-        id = 1
-        image_path = '/home/pi/Desktop/NTOU_CSE_LAB403/main/upload_frames/upload.png'
-        delete_data(id)
-        # 新增資料
-        insert_data(id, 'picture01', image_path)
+    # 若按下 q 鍵則離開迴圈
+    if cv2.waitKey(1000) & 0xFF == ord('q'):
+      break
+    else:
+      cv2.imwrite("/home/pi/Desktop/NTOU_CSE_LAB403/main/upload_frames/upload.png", frame1)
+      id = 1
+      image_path = '/home/pi/Desktop/NTOU_CSE_LAB403/main/upload_frames/upload.png'
+      delete_data(id)
+      # 新增資料
+      insert_data(id, 'picture01', image_path)
 
-    # 查詢資料
-    result = select_data(id)
-    if result[2] is not None:
-        print(result[0], result[1])
-        image_download = Image.open(io.BytesIO(result[2]))
-        image_download.show()
+  # 查詢資料
+  result = select_data(id)
+  if result[2] is not None:
+      print(result[0], result[1])
+      image_download = Image.open(io.BytesIO(result[2]))
+      image_download.show()
 
-    # 關閉資料庫連線
-    fishDB.close()
-    # 釋放攝影機
-    cap1.release()
-    #cap2.release()
-    # 關閉所有 OpenCV 視窗
-    cv2.destroyAllWindows()
+  # 關閉資料庫連線
+  fishDB.close()
+  # 釋放攝影機
+  cap1.release()
+  #cap2.release()
+  # 關閉所有 OpenCV 視窗
+  cv2.destroyAllWindows()
