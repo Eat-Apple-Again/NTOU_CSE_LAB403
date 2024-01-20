0276ef5e1388c1d2a7be0d96fc667baf1cba4ccd
Eat-Apple-Again <00757044@email.ntou.edu.tw>
2024-01-21 01:19:15 +0800

    Retrieve RTSP commands from the config

--- main/monitor.py
+++ main/monitor.py
@@ -1,22 +1,30 @@
 import cv2
+import json
 
 # JUST STREAMING
 # 參考 https://www.ispyconnect.com/camera/d-link
-# cap1 = cv2.VideoCapture('rtsp://admin:451466@192.168.1.51/live/profile.0') # 印表機
-# cap1 = cv2.VideoCapture('rtsp://admin:227182@192.168.1.21/live/profile.0') # 冷氣
-cap1 = cv2.VideoCapture('rtsp://Admin:1234@192.168.7.21/cam0/h264')
-cap1.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
+# cap = cv2.VideoCapture('rtsp://admin:451466@192.168.1.51/live/profile.0') # 印表機
+# cap = cv2.VideoCapture('rtsp://admin:227182@192.168.1.21/live/profile.0') # 冷氣
+# cap = cv2.VideoCapture('rtsp://Admin:1234@192.168.7.21/cam0/h264') # 現場Dynacolor
+
+with open('config.json', 'r') as config_file:
+    config_data = json.load(config_file)
+camera_config = config_data['camera_config']
+
+# 建立RTSP連線
+cap = cv2.VideoCapture(camera_config['dynacolor02'])
 
 i = 0
 while(True):
   # 從攝影機擷取一張影像
-  ret1, frame1 = cap1.read()
+  ret1, frame1 = cap.read()
 
   #resize to 720*480
   if ret1:
     frame1 = cv2.resize(frame1,(720,480))
   else:
     break
+
   if i == 0:
     print(frame1.shape)
     i = 1
@@ -31,7 +39,7 @@ while(True):
 
 
 # 釋放攝影機
-cap1.release()
+cap.release()
 
 # 關閉所有 OpenCV 視窗
 cv2.destroyAllWindows()
